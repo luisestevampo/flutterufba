c68cd254d40322e054c8efbc7800e4c4c549b417
Luis Estevam <luisestevamgp@gmail.com>
2024-06-19 01:22:24 -0300

    adicionando lógica do sistema

--- lib/main.dart
+++ lib/main.dart
@@ -68,12 +68,13 @@ class inicio extends StatelessWidget {
   }
 }
 
-class Questoes {
+class Question {
   String text;
-  List<String> opcoes;
-  int?OpcaoSelecionada;
+  List<String> options;
+  int?selectedOption;
+  int correctOption;
 
-  Questoes(this.text, this.opcoes, this.OpcaoSelecionada);
+  Question(this.text, this.options, this.selectedOption, this.correctOption);
 }
 
 class FirstPage extends StatefulWidget{
@@ -84,6 +85,38 @@ class FirstPage extends StatefulWidget{
 }
 
 class _FirstPageState extends State<FirstPage> {
+
+  List<Question> questions =[
+    Question('Descreva aqui a primeira pergunta:', ['a) Alternativa A', 'b) Alternativa B', 'c) Alternativa C'], null, 0),
+    Question('Descreva aqui a primeira pergunta:', ['a) Alternativa A', 'b) Alternativa B', 'c) Alternativa C'], null, 0),
+    Question('Descreva aqui a primeira pergunta:', ['a) Alternativa A', 'b) Alternativa B', 'c) Alternativa C'], null, 0),
+    Question('Descreva aqui a primeira pergunta:', ['a) Alternativa A', 'b) Alternativa B', 'c) Alternativa C'], null, 0),
+    Question('Descreva aqui a primeira pergunta:', ['a) Alternativa A', 'b) Alternativa B', 'c) Alternativa C'], null,0),
+  ];
+
+  void calculateAndNavigate() {
+    int score = 0;
+    for (var question in questions) {
+      if (question.selectedOption == question.correctOption) {
+        score++;
+      }
+    }
+    double grade = (score / questions.length) * 10;
+
+    bool isApproved = grade >= 6; 
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => TelaResultado(
+          grade: grade,
+          isApproved: isApproved,
+        ),
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,126 +127,78 @@ class _FirstPageState extends State<FirstPage> {
       ),
 
 
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Questão 1"),
-            Text("Questão 2"),
-            Text("Questão 3"),
-            Text("Questão 4"),
-            Text("Questão 5"),
-          ],
-          
-        )
-        
-      ),
-
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_back),
-            label: 'Voltar'
-            ),
-            
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home'
-
-            ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_forward),
-            label: 'Próxima'
-            )
-      ],
-      currentIndex: 0,
-      onTap: (index) {
-        if (index == 2) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const TelaResultado()),
-          ); 
-        } else if (index == 0 ) {
-            Navigator.pop(
-            context,
-            MaterialPageRoute(builder: (context) => const inicio()),
+      body: Center(
+        child: ListView.builder(
+          itemCount: questions.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Card(
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        questions[index].text,
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      for (int i = 0; i < questions[index].options.length; i++)
+                        RadioListTile<int>(
+                          title: Text(questions[index].options[i]),
+                          value: i,
+                          groupValue: questions[index].selectedOption,
+                          onChanged: (value) {
+                            setState(() {
+                              questions[index].selectedOption = value;
+                            });
+                          },
+                        ),
+                    ],
+                  ),
+                ),
+              ),
             );
-        } else {
-          Navigator.pop(
-            context,
-            MaterialPageRoute(builder: (context) => const inicio())
-          );
-        }
-        
-      },
+          },
+        ),
+      ),
+       bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ElevatedButton(
+            onPressed: calculateAndNavigate,
+            child: Text('Enviar Respostas'),
+          ),
+        ),
       ),
     );
   }
 }
 
 
-class TelaResultado extends StatefulWidget {
-  const TelaResultado({super.key});
+class TelaResultado extends StatelessWidget {
+  const TelaResultado({super.key, required this.grade, required this.isApproved});
+  final double grade;
+  final bool isApproved;
 
-  @override
-  _TelaResultadoState createState() => _TelaResultadoState();
-}
-
-class _TelaResultadoState extends State<TelaResultado> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Página de Resultado"),
-        backgroundColor: Colors.green,
+        backgroundColor: isApproved ? Colors.green : Colors.red,
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Página de resutado"),
+      body: Center(
+        child: Text(
+          isApproved
+              ? 'Aluno aprovado com nota $grade'
+              : 'Aluno reprovado com nota $grade',
+          textAlign: TextAlign.center,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_back),
-            label: 'Voltar'
-            ),
-            
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home'
-
-            ),
-        ],
-        currentIndex: 0,
-      onTap: (index) {
-        if (index == 0) {
-          Navigator.pop(
-            context,
-            MaterialPageRoute(builder: (context) => const FirstPage()),
-          ); 
-        } else if (index == 1 ) {
-            Navigator.pop(
-            context,
-            MaterialPageRoute(builder: (context) => const inicio()),
-            );
-        }
-      },
       ),
+      
     );
   }
 }
-
-class Applogic {
-  /* 
-    criar 5 questões com botão radio (Leonardo)
-    setar que a alternativa correta é sempre a letra A
-
-
-    Cada acerto conta pontuação ++
-    if numero de acertos for =>3 exibe tela verde com a mennsagem de aprovado  e pontuação
-    else exibe tela ver melha com a mensagem de reprovado ee pontuação
-  
-  
-   */
-}
-
