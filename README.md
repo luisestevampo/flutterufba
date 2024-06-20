c9dcca0cf86e87f97bcdb68e045b4713ecf72213
Luis Estevam <luisestevamgp@gmail.com>
2024-06-20 10:13:10 -0300

    adicionando imagens e arquivo readm.md

--- README.md
+++ README.md
@@ -2,15 +2,55 @@
 
 A new Flutter project.
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+
+
+
+
+
+# Projeto
+
+## Informações básicas
+
+Em equipe, desenvolva um aplicativo em Flutter que possa ser executado no Google Chrome.
+
+## Equipes
+
+Luis Fernando dos Santos Estevam
+Leonardo v. Tourinho
+
+## Restrições
+
+O aplicativo deve:
+
+- possuir ao menos duas telas
+- ser interativo
+- usar persistência de dados (SQLite, SharedPreferences, etc.)
+
+## Entrega
+
+Enviar arquivo `.zip` do aplicativo para rodrigorgs@gmail.com. O aplicativo deve conter:
+
+- arquivo `README.md` contendo:
+  - descrição do projeto
+  - instruções para compilar e executar o projeto
+  - **lista de membros da equipe**
+- código-fonte do projeto
+- NÃO inclua as pastas `build`, `ios`, `android`, `windows`, `linux` e `macos`
+- pasta `screenshots` contendo **screenshots** de todas as telas do aplicativo, no formato PNG
+
+## Prazo
+
+- Quinta-feira, 20 de junho, 11:00
+
+## Avaliação
+
+A avaliação será feita com base nos seguintes critérios:
+
+- Funcionalidade e ausência de bugs
+- Qualidade da interface
+- Qualidade do código (avaliada com `flutter analyze`)
+- Aderência às orientações desta página
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: ("App Gabaritando"),
       home: const inicio(),
-      );
+    );
   }
 }
 
@@ -20,50 +20,44 @@ class inicio extends StatelessWidget {
   const inicio({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text ("App Gabaritando"),
+        title: const Text("App Gabaritando"),
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const FirstPage()));
-              }, 
-              
-            child: const Text("Iniciar teste"))
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const FirstPage()));
+                },
+                child: const Text("Iniciar teste"))
           ],
         ),
-    ),
-    bottomNavigationBar: BottomNavigationBar(
+      ),
+      bottomNavigationBar: BottomNavigationBar(
         items: const [
-
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home'
-
-            ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_forward),
-            label: 'Próxima'
-            )
+              icon: Icon(Icons.arrow_forward), label: 'Próxima')
         ],
-         currentIndex: 0,
-      onTap: (index) {
-        if (index == 1) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const FirstPage()),
-          ); 
-        } 
+        currentIndex: 0,
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const FirstPage()),
+            );
+          }
         },
-    ),
+      ),
     );
   }
 }
@@ -71,26 +65,66 @@ class inicio extends StatelessWidget {
 class Question {
   String text;
   List<String> options;
-  int?selectedOption;
+  int? selectedOption;
   int correctOption;
 
   Question(this.text, this.options, this.selectedOption, this.correctOption);
 }
 
-class FirstPage extends StatefulWidget{
-  const FirstPage ({super.key});
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
 
   @override
   State<FirstPage> createState() => _FirstPageState();
 }
 
-class _FirstPageState extends State<FirstPage>{
-  List<Question> questions =[
-    Question('1. O que é Flutter?:', ['a) Flutter é um framework de código aberto desenvolvido pelo Google para criar interfaces de usuário nativas de alta qualidade em plataformas móveis (iOS e Android) e web a partir de uma única base de código.', 'b) Flutter utiliza a linguagem java e Dotnet para escrever aplicativos, não ferecendo um ambiente de desenvolvimento rápido e eficiente.', 'c) É conhecido por sua performance lenta e por proporcionar uma experiência visual pouco atrativa através de seus widgets.'], null, 0),
-    Question('2. Quais são as vantagens de usar Flutter?:', ['a)Desenvolvimento rápido de aplicativos para múltiplas plataformas a partir de um único código-fonte.', 'b) Widgets personalizáveis e de baixo desempenho que oferecem uma experiência para poucos programadores.', 'c) Pouca comunidade de desenvolvedores e sem suporte do Google.'], null, 0),
-    Question('3. Quais são os principais componentes do Flutter?:', ['a) Widgets: São os blocos de construção básicos da interface do usuário no Flutter, que juntos formam a estrutura visual dos aplicativos.', 'b) java basico e c#: São as diretrizes de design do Flutter para criar aplicativos.', 'c) Clipper: A linguagem de programação utilizada pelo Flutter, conhecida por sua sintaxe Complexa para aplicativos móveis.'], null, 0),
-    Question('4. Como o Flutter se compara a outras tecnologias de desenvolvimento móvel?:', ['a) Ao contrário de abordagens baseadas em WebView, como o Cordova, Flutter produz aplicativos nativos que não dependem de navegadores para renderização.', 'b)  Em comparação com frameworks como React Native, Flutter geralmente oferece menos performance e pouca experiência de usuário mais consistente. .', 'c) A curva de aprendizado pode ser mais complexo devido a uma linguagem sem suporte .'], null, 0),
-    Question('5. Quais são os casos de uso ideais para Flutter?:', ['a) Desenvolvimento de aplicativos que precisam ser lançados rapidamente em várias plataformas sem comprometer a performance.', 'b)Projetos que exigem interfaces pouco personalizáveis e visualmente .', 'c) Aplicativos que necessitam de suporte contínuo e atualizações demoradas e recursos pouco eficiente.'], null,0),
+class _FirstPageState extends State<FirstPage> {
+  List<Question> questions = [
+    Question(
+        '1. O que é Flutter?:',
+        [
+          'a) Flutter é um framework de código aberto desenvolvido pelo Google para criar interfaces de usuário nativas de alta qualidade em plataformas móveis (iOS e Android) e web a partir de uma única base de código.',
+          'b) Flutter utiliza a linguagem java e Dotnet para escrever aplicativos, não ferecendo um ambiente de desenvolvimento rápido e eficiente.',
+          'c) É conhecido por sua performance lenta e por proporcionar uma experiência visual pouco atrativa através de seus widgets.'
+        ],
+        null,
+        0),
+    Question(
+        '2. Quais são as vantagens de usar Flutter?:',
+        [
+          'a)Desenvolvimento rápido de aplicativos para múltiplas plataformas a partir de um único código-fonte.',
+          'b) Widgets personalizáveis e de baixo desempenho que oferecem uma experiência para poucos programadores.',
+          'c) Pouca comunidade de desenvolvedores e sem suporte do Google.'
+        ],
+        null,
+        0),
+    Question(
+        '3. Quais são os principais componentes do Flutter?:',
+        [
+          'a) Widgets: São os blocos de construção básicos da interface do usuário no Flutter, que juntos formam a estrutura visual dos aplicativos.',
+          'b) java basico e c#: São as diretrizes de design do Flutter para criar aplicativos.',
+          'c) Clipper: A linguagem de programação utilizada pelo Flutter, conhecida por sua sintaxe Complexa para aplicativos móveis.'
+        ],
+        null,
+        0),
+    Question(
+        '4. Como o Flutter se compara a outras tecnologias de desenvolvimento móvel?:',
+        [
+          'a) Ao contrário de abordagens baseadas em WebView, como o Cordova, Flutter produz aplicativos nativos que não dependem de navegadores para renderização.',
+          'b)  Em comparação com frameworks como React Native, Flutter geralmente oferece menos performance e pouca experiência de usuário mais consistente. .',
+          'c) A curva de aprendizado pode ser mais complexo devido a uma linguagem sem suporte .'
+        ],
+        null,
+        0),
+    Question(
+        '5. Quais são os casos de uso ideais para Flutter?:',
+        [
+          'a) Desenvolvimento de aplicativos que precisam ser lançados rapidamente em várias plataformas sem comprometer a performance.',
+          'b)Projetos que exigem interfaces pouco personalizáveis e visualmente .',
+          'c) Aplicativos que necessitam de suporte contínuo e atualizações demoradas e recursos pouco eficiente.'
+        ],
+        null,
+        0),
   ];
 
   void calculateAndNavigate() {
@@ -102,7 +136,7 @@ class _FirstPageState extends State<FirstPage>{
     }
     double grade = (score / questions.length) * 10;
 
-    bool isApproved = grade >= 6; 
+    bool isApproved = grade >= 6;
 
     Navigator.push(
       context,
@@ -116,15 +150,13 @@ class _FirstPageState extends State<FirstPage>{
   }
 
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text ("App Gabaritando"),
+        title: const Text("App Gabaritando"),
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-
       body: Center(
         child: ListView.builder(
           itemCount: questions.length,
@@ -140,7 +172,8 @@ class _FirstPageState extends State<FirstPage>{
                     children: [
                       Text(
                         questions[index].text,
-                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
                       for (int i = 0; i < questions[index].options.length; i++)
                         RadioListTile<int>(
@@ -161,48 +194,38 @@ class _FirstPageState extends State<FirstPage>{
           },
         ),
       ),
-
       bottomNavigationBar: BottomNavigationBar(
         items: const [
-
           BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_back),
-            label: 'Voltar'
-            ),
-
+              icon: Icon(Icons.arrow_back), label: 'Voltar'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home'
-            ),
-
-            BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_forward),
-            label: 'Próxima'
-            )
+              icon: Icon(Icons.arrow_forward), label: 'Próxima')
         ],
-         currentIndex: 0,
-          onTap: (index) {
+        currentIndex: 0,
+        onTap: (index) {
           if (index == 0) {
-          Navigator.pop(
-            context,
-            MaterialPageRoute(builder: (context) => const inicio()),
-          ); 
-        } else if(index==1) {
-          Navigator.pop(
-            context,
-            MaterialPageRoute(builder: (context) => const inicio()),
-          );
-        } else {
-           calculateAndNavigate();
+            Navigator.pop(
+              context,
+              MaterialPageRoute(builder: (context) => const inicio()),
+            );
+          } else if (index == 1) {
+            Navigator.pop(
+              context,
+              MaterialPageRoute(builder: (context) => const inicio()),
+            );
+          } else {
+            calculateAndNavigate();
           }
         },
-    ),
+      ),
     );
   }
 }
 
 class TelaResultado extends StatelessWidget {
-  const TelaResultado({super.key, required this.grade, required this.isApproved});
+  const TelaResultado(
+      {super.key, required this.grade, required this.isApproved});
   final double grade;
   final bool isApproved;
 
@@ -220,36 +243,29 @@ class TelaResultado extends StatelessWidget {
               ? 'Aluno aprovado com nota $grade'
               : 'Aluno reprovado com nota $grade',
           textAlign: TextAlign.center,
-      ),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
-
           BottomNavigationBarItem(
-            icon: Icon(Icons.arrow_back),
-            label: 'Voltar'
-            ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home'
-            ),       
+              icon: Icon(Icons.arrow_back), label: 'Voltar'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
         ],
-         currentIndex: 0,
-          onTap: (index) {
+        currentIndex: 0,
+        onTap: (index) {
           if (index == 0) {
-          Navigator.pop(
-            context,  
-            MaterialPageRoute(builder: (context) => const FirstPage()),
-          ); 
-        } else if (index == 1) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const inicio()),
-          );
-        }
+            Navigator.pop(
+              context,
+              MaterialPageRoute(builder: (context) => const FirstPage()),
+            );
+          } else if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const inicio()),
+            );
+          }
         },
-    ),
+      ),
     );
   }
 }
